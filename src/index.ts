bc06a4bf73d63eec12074e45047772e5cba1ddcc
meows <b5c6@protonmail.com>
2020-05-11 06:03:54 -0500

    cleanup syntax
    Signed-off-by: meows <b5c6@protonmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,82 +1,80 @@
 #!/usr/bin/env node
 
-import { JsonSchemaToTypes } from "@etclabscore/json-schema-to-types";
-import { JSONSchema } from "@open-rpc/meta-schema";
-
-const { program } = require('commander');
+const {program} = require('commander');
 const fs = require('fs');
 
+import {JsonSchemaToTypes} from "@etclabscore/json-schema-to-types";
+import {JSONSchema} from "@open-rpc/meta-schema";
+
 program.version("0.0.1");
-program.option("-l, --target <target>", "transpilation target language" )
+program.option("-l, --target <target>", "target language")
 program.option("-v, --verbose", "verbose logging");
 program.parse(process.argv);
 
-// Get either stdin or first argument.
-// https://gist.github.com/ishu3101/9fa58ca1440f780d6288
-var args = program.args; // process.argv.slice(2);
-var input = args[0];
-var isTTY = process.stdin.isTTY;
-var stdin = process.stdin;
-var stdout = process.stdout;
+let args = program.args;
+let input = args[0];
+let isTTY = process.stdin.isTTY;
+let stdin = process.stdin;
 
-// If no STDIN and no arguments, display usage message
 if (isTTY && args.length === 0) {
-    program.help();
-}
-// If no STDIN but arguments given
-else if (isTTY && args.length !== 0) {
-    handleShellArguments();
-}
-// read from STDIN
-else {
-    handlePipedContent();
+  program.help();
+} else if (isTTY && args.length !== 0) {
+  handleShellArguments();
+} else {
+  handlePipedContent();
 }
 
 function handlePipedContent() {
-    var data = '';
-    stdin.on('readable', function() {
-        var chuck = stdin.read();
-        if(chuck !== null){
-            data += chuck;
-        }
-    });
-    stdin.on('end', function() {
-        handleData(data);
-    });
+  let data = '';
+  stdin.on('readable', function () {
+    let chuck = stdin.read();
+    if (chuck !== null) {
+      data += chuck;
+    }
+  });
+  stdin.on('end', function () {
+    handleData(data);
+  });
 }
 
-function handleShellArguments(){
-    let rawData = fs.readFileSync(input);
-    handleData(rawData);
+function handleShellArguments() {
+  let rawData = fs.readFileSync(input);
+  handleData(rawData);
 }
 
 function handleData(input: string) {
   if (input === "") {
+    program.help();
+    process.exit(2);
+  }
+
+  if (typeof (program.target) === "undefined" || program.target === "") {
+    console.log("missing target target option");
+    program.help();
     process.exit(2);
   }
 
   const s = JSON.parse(input) as JSONSchema;
   const transpiler = new JsonSchemaToTypes(s);
 
-    if (typeof(program.target) === "undefined" || program.target === "") {
-        console.log("missing target target option");
-        program.help();
-        process.exit(2);
-    }
-
-    const target = program.target;
-    if (target === "rs") {
-        console.log(transpiler.toRust());
-    } else if (target === "go") {
-        console.log(transpiler.toGo());
-    }  else if (target === "ts") {
-        console.log(transpiler.toTypescript());
-    }  else if (target === "py") {
-        console.log(transpiler.toPython());
-    } else {
-        console.log("invalid target");
-        program.help();
-        process.exit(2);
-    }
+  const target = program.target;
+  switch (target) {
+    case "rs":
+      console.log(transpiler.toRust());
+      return
+    case "go":
+      console.log(transpiler.toGo());
+      return
+    case "ts":
+      console.log(transpiler.toTypescript());
+      return
+    case "py":
+      console.log(transpiler.toPython());
+      return
+    default:
+      console.log("invalid target");
+      program.help();
+      process.exit(2);
+  }
 }
 
